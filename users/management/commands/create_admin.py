0351f4da3465174acfb43967c8a2c0907ef9998e
Spacedeity <life312line@gmail.com>
2026-07-24 18:40:23 +0300

    Add create_admin management command

---
+++ users/management/commands/create_admin.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Create admin user if it doesn't exist"
+
+    def handle(self, *args, **kwargs):
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Admin123456!")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password,
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created."))
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists."))
